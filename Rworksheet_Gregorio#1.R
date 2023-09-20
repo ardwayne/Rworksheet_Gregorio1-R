7b81cc7a16106cb9eea2b6240dc515213202a27c
ardwayne <ardwayne.gregorio@students.isatu.edu.ph>
2023-09-20 04:05:25 +0000

    commit

---
+++ Rworksheet_Gregorio#1.R
@@ -0,0 +1,45 @@
+ageVector <- c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27, 22, 
+               37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 42, 53, 41, 
+               51, 35, 24, 33, 41)
+length(ageVector)
+
+#2
+reciprocalageVector <- 1 / age
+reciprocalageVector
+
+#3
+new_ageVector <- c(ageVector, 0, ageVector)
+new_ageVector
+
+#4
+sortadageVector <- sort(ageVector)
+sortadageVector
+
+#5
+min_ageVector <- min(ageVector)
+min_ageVector
+max_ageVector <- max(ageVector)
+max_ageVector                     
+
+#6                     
+dataVector <- c(2.4, 2.8, 2.1, 2.5, 2.4, 2.2, 2.5, 2.3,
+          2.5, 2.3, 2.4, 2.7)
+length(dataVector)
+
+#7
+doubled_datavector <- c (dataVector * 2)
+doubled_datavector
+
+#8
+seq1 <- seq(1, 100)
+seq1
+
+seq2 <- seq(20, 60)
+seq2
+
+mean_seq <- mean(seq2)
+mean_seq
+
+sum_seq3 <- sum(seq(51, 91))
+sum_seq3
+
